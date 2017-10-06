58d09f616c7f3d87a06a64e50abcf8773ea3ca71
Tom Moir <trmoir9@gmail.com>
2017-10-06 13:44:26 +0100

    Added students to array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
+students = [
+	"Dr. Hannibal Lecter",
+	"Darth Vader",
+	"Nurse Ratched",
+	"Michael Corleone",
+	"Alex DeLarge",
+	"The Wicked Witch of the West",
+	"Terminator",
+	"Freddy Krueger",
+	"The Joker",
+	"Joffrey Baratheon",
+	"Norman Bates"
+]
+student_count = students.length
+
 puts "The Students of Villains Academy:"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-
-print "Overall, we have "
-print 11
-puts " great students"
+puts students
+puts "Overall, we have #{student_count} great students"
