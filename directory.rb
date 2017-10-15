ecf1b7442707d3d764e59a579f77a141bdea4d4f
Tom Moir <trmoir9@gmail.com>
2017-10-15 20:46:39 +0100

    Added method to choose how you sort / filter the list of students you would like printed.

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
 def input_students
 	students = []
-	puts "Please enter the name of the students."
+	puts "Please enter the student information"
 	puts "Hit Enter with no input to finish."
+	puts
 	loop do 
+		puts "Please enter full name."
 		name = gets.chomp
 		puts
 		break if name.empty?
-		puts "Please enter cohort"
+		puts "Please enter cohort."
 		cohort = gets.chomp
+		cohort = cohort.empty? ? 'October' : cohort
 		puts
 		puts "Please enter DOB (DD/MM/YYYY)"
 		dob = gets.chomp
@@ -26,29 +29,54 @@ def print_header(headline)
 	puts "-------------".center(width)
 end
 
-def print_all_names(students)
+def print_all(selection)
 	# => Using each with index:
-	# students.each_with_index { |student, index| puts "#{index + 1}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)" }
+	print_header("The Students of Makers Academy:")
+	unless selection.empty?
+		selection.each_with_index { |student, index| puts "#{index + 1}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)" }
+	else
+		puts "No students in current selection".center(50)
+	end
 
 	# => Using loop:
-	iter = 0
-	while iter < students.length
-		student = students[iter]
-		iter += 1
-		puts "#{iter}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)"
+	# iter = 0
+	# while iter < students.length
+	# 	student = students[iter]
+	# 	iter += 1
+	# 	puts "#{iter}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)"
+	# end
+end
+
+def print_cohort(students)
+	# => Print list of students from a specific cohort
+	puts "Which cohort would you like to sort by?"
+	cohort = gets.chomp
+	puts
+	selection = students.select { |student| student[:cohort] == cohort }
+	print_all(selection)
+end
+
+def print_with_initial(students)
+	# => Print if letter begins with chosen initial.
+	puts "Which initial would you like to sort by?"
+	initial = gets.chomp.downcase
+	puts
+	if initial.length == 1
+		selection = students.select { |student| student[:name][0].downcase == initial }
+		print_all(selection)
+	else
+		puts "Please enter 1 initial."
+		print_with_initial(students)
 	end
 end
 
-def print_with_initial(students, initial)
-	# => Print if letter begins with chosen initial.
-	selection = students.select { |student| student[:name][0].downcase == initial.downcase }
-	selection.each_with_index { |student, index| puts "#{index + 1}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)" }
-end
-
-def print_names_less_than(students, x)
+def print_names_less_than(students)
 	# => Print names with less than x characters
-	selection = students.select { |student| student[:name].length < x }
-	selection.each_with_index { |student, index| puts "#{index + 1}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)" }
+	puts "What's the maximum number of characters in a name you'd like to see?"
+	num = gets.chomp.to_i
+	puts
+	selection = students.select { |student| student[:name].length < num }
+	print_all(selection)
 end
 
 def print_footer(students)
@@ -61,10 +89,40 @@ def print_footer(students)
 	end
 end
 
-students = input_students
-print_header("The Students of Makers Academy:")
-print_all_names(students)
-print_footer(students)
+def sort?
+	puts "Would you like to sort / filter your list of students? [y / n]"
+	input = gets.chomp.downcase
+	puts
+	if input == 'y'
+		true
+	elsif input == 'n'
+		false
+	else
+		puts "Not a valid repsonse. Please answer with 'y' or 'n'"
+		sort?
+	end
+end
 
-# print_with_initial(students, 'a')
-# print_names_less_than(students, 5)
+def get_sort
+	puts "How would you like to sort the list of students?"
+	puts "Options: cohort / initial / characters"
+	input = gets.chomp.downcase
+	puts
+	if ['cohort', 'initial', 'characters'].include?(input)
+		input
+	else
+		puts "#{input} is not a valid option."
+		get_sort
+	end
+end
+
+students = input_students
+print_what = sort? ? get_sort : 'all'
+case print_what
+when 'cohort' then print_cohort(students)
+when 'initial' then print_with_initial(students)
+when 'characters' then print_names_less_than(students)
+else
+	print_all(students)
+end
+print_footer(students)
