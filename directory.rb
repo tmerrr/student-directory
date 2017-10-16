0137630cdf156c063405a8db19cd3f40d28937fe
Tom Moir <trmoir9@gmail.com>
2017-10-16 09:55:31 +0100

    Refactored menu method

--- directory.rb
+++ directory.rb
@@ -1,34 +1,40 @@
 def menu
-	# 1. Print options to user
-	# 2. Take an input and store in variable
-	# 3. Complete an action based on the variable
-	# 4. repeat.
-	students = []
+	@students = []
 	loop do
-		puts "What would you like to do next?"
-		puts "1. Input students"
-		puts "2. Print students"
-		puts "9. Exit program"
-		input = gets.chomp
-		puts
-		case input
-		when '1' then input_students(students)
-		when '2'
-			case get_sort
-			when 'all' then print_names(students, students)
-			when 'cohort' then print_cohort(students)
-			when 'initial' then print_with_initial(students)
-			when 'characters' then print_max_chars(students)
-			end
-		when '9'
-			abort('Program Ended')
-		else
-			puts "#{input} is not a valid entry"
-		end
+		print_menu_options
+		menu_selection(gets.chomp)
 	end
 end
 
-def input_students(students)
+def print_menu_options
+	puts "What would you like to do next?"
+	puts "1. Input students"
+	puts "2. Print students"
+	puts "9. Exit program"
+end
+
+def menu_selection(user_input)
+	puts
+	case user_input
+	when '1' then input_students
+	when '2' then sort_students
+	when '9' then abort('Program Ended')
+	else
+		puts "#{user_input} is not a valid entry"
+	end
+end
+
+def sort_students
+	case get_sort
+	when 'all' then print_names(@students)
+	when 'cohort' then print_cohort
+	when 'initial' then print_with_initial
+	when 'characters' then print_max_chars
+	end
+	print_footer
+end
+
+def input_students
 	puts "Please enter the student information:"
 	puts "Enter a blank name to exit."
 	puts
@@ -39,9 +45,9 @@ def input_students(students)
 		break if name.empty?
 		puts "Please enter cohort."
 		cohort = get_month
-		students << {name: name, cohort: cohort}
+		@students << {name: name, cohort: cohort}
 		puts "#{name} has been added."
-		puts "We now have a total of #{students.count} #{students.count > 1 ? 'students' : 'student'}."
+		puts "We now have a total of #{@students.count} #{@students.count > 1 ? 'students' : 'student'}."
 		puts
 	end
 end
@@ -68,7 +74,7 @@ def print_header(headline)
 	puts "-------------".center(width)
 end
 
-def print_names(selection, all_students)
+def print_names(selection)
 	width = 50
 	# => Using each with index:
 	print_header("The Students of Makers Academy:")
@@ -80,58 +86,48 @@ def print_names(selection, all_students)
 	else
 		puts "No students in current selection".center(width)
 	end
-
-	print_footer(all_students)
-
-	# => Using loop:
-	# iter = 0
-	# while iter < students.length
-	# 	student = students[iter]
-	# 	iter += 1
-	# 	puts "#{iter}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)"
-	# end
 end
 
-def print_cohort(students)
+def print_cohort
 	# => Print list of students from a specific cohort
 	puts "Which cohort would you like to sort by?"
 	cohort = get_month
 	puts
-	selection = students.select { |student| student[:cohort] == cohort }
-	print_names(selection, students)
+	selection = @students.select { |student| student[:cohort] == cohort }
+	print_names(selection)
 end
 
-def print_with_initial(students)
+def print_with_initial
 	# => Print if letter begins with chosen initial.
 	puts "Which initial would you like to sort by?"
 	initial = gets.chomp.downcase
 	puts
 	if initial.length == 1
-		selection = students.select { |student| student[:name][0].downcase == initial }
-		print_names(selection, students)
+		selection = @students.select { |student| student[:name][0].downcase == initial }
+		print_names(selection)
 	else
 		puts "Please enter 1 initial."
-		print_with_initial(students)
+		print_with_initial
 	end
 end
 
-def print_max_chars(students)
+def print_max_chars
 	# => Print names with less than x characters
 	puts "What's the maximum number of characters in a name you'd like to see?"
 	num = gets.chomp.to_i
 	puts
-	selection = students.select { |student| student[:name].length <= num }
-	print_names(selection, students)
+	selection = @students.select { |student| student[:name].length <= num }
+	print_names(selection)
 end
 
-def print_footer(students)
+def print_footer
 	width = 50
 	puts "-------------".center(width)
-	if students.empty?
+	if @students.empty?
 		puts "No students have been added to the directory.".center(width)
 		puts
 	else
-		puts "Overall, we have #{students.length} great #{students.length > 1 ? 'students' : 'student'}".center(width)
+		puts "Overall, we have #{@students.length} great #{@students.length > 1 ? 'students' : 'student'}".center(width)
 		puts
 	end
 end
