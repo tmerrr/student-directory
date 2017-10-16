ba54c18934c014acf29a3fa9c68c81c11c670fab
Tom Moir <trmoir9@gmail.com>
2017-10-16 09:33:52 +0100

    Completed interactive menu and multiple sort options

--- directory.rb
+++ directory.rb
@@ -1,7 +1,36 @@
-def input_students
+def menu
+	# 1. Print options to user
+	# 2. Take an input and store in variable
+	# 3. Complete an action based on the variable
+	# 4. repeat.
 	students = []
-	puts "Please enter the student information"
-	puts "Hit Enter with no input to finish."
+	loop do
+		puts "What would you like to do next?"
+		puts "1. Input students"
+		puts "2. Print students"
+		puts "9. Exit program"
+		input = gets.chomp
+		puts
+		case input
+		when '1' then input_students(students)
+		when '2'
+			case get_sort
+			when 'all' then print_names(students, students)
+			when 'cohort' then print_cohort(students)
+			when 'initial' then print_with_initial(students)
+			when 'characters' then print_max_chars(students)
+			end
+		when '9'
+			abort('Program Ended')
+		else
+			puts "#{input} is not a valid entry"
+		end
+	end
+end
+
+def input_students(students)
+	puts "Please enter the student information:"
+	puts "Enter a blank name to exit."
 	puts
 	loop do 
 		puts "Please enter full name."
@@ -9,18 +38,28 @@ def input_students
 		puts
 		break if name.empty?
 		puts "Please enter cohort."
-		cohort = gets.chomp
-		cohort = cohort.empty? ? 'October' : cohort
-		puts
-		puts "Please enter DOB (DD/MM/YYYY)"
-		dob = gets.chomp
-		puts
-		students << {name: name, cohort: cohort, dob: dob}
+		cohort = get_month
+		students << {name: name, cohort: cohort}
 		puts "#{name} has been added."
 		puts "We now have a total of #{students.count} #{students.count > 1 ? 'students' : 'student'}."
 		puts
 	end
-	students
+end
+
+def get_month
+	months = [	'January', 'February', 'March', 'April', 'May', 'June',
+				'July', 'August', 'September', 'October', 'November', 'December']
+	input = gets.chomp.capitalize
+	puts
+	if input.empty?
+		# => Default cohort.
+		'October'
+	elsif months.include?(input)
+		input
+	else
+		puts "#{input} is not a valid month. Please try again."
+		get_month
+	end
 end
 
 def print_header(headline)
@@ -29,14 +68,20 @@ def print_header(headline)
 	puts "-------------".center(width)
 end
 
-def print_all(selection)
+def print_names(selection, all_students)
+	width = 50
 	# => Using each with index:
 	print_header("The Students of Makers Academy:")
 	unless selection.empty?
 		selection.each_with_index { |student, index| puts "#{index + 1}.  #{student[:name]} (#{student[:cohort].capitalize} cohort)" }
+		puts
+		puts "#{selection.count} #{selection.count > 1 ? 'students' : 'student'} in the current selection".center(width)
+		puts
 	else
-		puts "No students in current selection".center(50)
+		puts "No students in current selection".center(width)
 	end
+
+	print_footer(all_students)
 
 	# => Using loop:
 	# iter = 0
@@ -50,10 +95,10 @@ end
 def print_cohort(students)
 	# => Print list of students from a specific cohort
 	puts "Which cohort would you like to sort by?"
-	cohort = gets.chomp
+	cohort = get_month
 	puts
 	selection = students.select { |student| student[:cohort] == cohort }
-	print_all(selection)
+	print_names(selection, students)
 end
 
 def print_with_initial(students)
@@ -63,20 +108,20 @@ def print_with_initial(students)
 	puts
 	if initial.length == 1
 		selection = students.select { |student| student[:name][0].downcase == initial }
-		print_all(selection)
+		print_names(selection, students)
 	else
 		puts "Please enter 1 initial."
 		print_with_initial(students)
 	end
 end
 
-def print_names_less_than(students)
+def print_max_chars(students)
 	# => Print names with less than x characters
 	puts "What's the maximum number of characters in a name you'd like to see?"
 	num = gets.chomp.to_i
 	puts
-	selection = students.select { |student| student[:name].length < num }
-	print_all(selection)
+	selection = students.select { |student| student[:name].length <= num }
+	print_names(selection, students)
 end
 
 def print_footer(students)
@@ -84,31 +129,19 @@ def print_footer(students)
 	puts "-------------".center(width)
 	if students.empty?
 		puts "No students have been added to the directory.".center(width)
+		puts
 	else
 		puts "Overall, we have #{students.length} great #{students.length > 1 ? 'students' : 'student'}".center(width)
-	end
-end
-
-def sort?
-	puts "Would you like to sort / filter your list of students? [y / n]"
-	input = gets.chomp.downcase
-	puts
-	if input == 'y'
-		true
-	elsif input == 'n'
-		false
-	else
-		puts "Not a valid repsonse. Please answer with 'y' or 'n'"
-		sort?
+		puts
 	end
 end
 
 def get_sort
 	puts "How would you like to sort the list of students?"
-	puts "Options: cohort / initial / characters"
+	puts "Options: all / cohort / initial / characters"
 	input = gets.chomp.downcase
 	puts
-	if ['cohort', 'initial', 'characters'].include?(input)
+	if ['all', 'cohort', 'initial', 'characters'].include?(input)
 		input
 	else
 		puts "#{input} is not a valid option."
@@ -116,13 +149,4 @@ def get_sort
 	end
 end
 
-students = input_students
-print_what = sort? ? get_sort : 'all'
-case print_what
-when 'cohort' then print_cohort(students)
-when 'initial' then print_with_initial(students)
-when 'characters' then print_names_less_than(students)
-else
-	print_all(students)
-end
-print_footer(students)
+menu
